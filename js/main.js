845d340d571f31a6f2d8287d84da8021a490d297
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-07 17:26:07 +0200

    Add form to sign up in studyGroup

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var student = {
     "firstName" : "Francesco",
     "lastName" : "Laurora",
-    "age" : 32,
+    "age" : 32
 };
 
 //stampo le informazioni contenute nell'oggetto attraverso il ciclo for-in
@@ -15,31 +15,31 @@ var studyGroup = [
     {
         "firstName" : "Leonardo",
         "lastName" : "Da Vinci",
-        "age" : 67,
+        "age" : 67
     },
 
     {
         "firstName" : "Raffaello",
         "lastName" : "Sanzio",
-        "age" : 37,
+        "age" : 37
     },
     
     {
         "firstName" : "Donatello",
         "lastName" : "di Betto Bardi",
-        "age" : 80,
+        "age" : 80
     },
 
     {
         "firstName" : "Michelangelo",
         "lastName" : "Buonarroti",
-        "age" : 88,
+        "age" : 88
     },        
 
     {
         "firstName" : "Maestro",
         "lastName" : "Splinter",
-        "age" : 37,
+        "age" : 37
     },
 ];
 
@@ -48,6 +48,33 @@ for(let i = 0; i < studyGroup.length; i++) {
     document.getElementById("ninja_turtles").innerHTML += `
     <li><strong>Nome:</strong> ${studyGroup[i].firstName}</li>
     <li><strong>Cognome:</strong> ${studyGroup[i].lastName}</li>
-    <hr>`
-
+    <hr>`;
 }
+
+//raccolgo le informazioni inserite dall'utente per creare un nuovo studente nell'array studyGroup
+var signUp = document.getElementById("sign_up");
+
+signUp.addEventListener("click",
+    function() {
+        //raccolgo gli input dell'utente
+        let firstName = document.getElementById("first_name").value;
+        let lastName = document.getElementById("last_name").value;
+        let age = parseInt(document.getElementById("age").value);
+
+        //creo un nuovo oggetto con gli input raccolti
+        let newStudent = {
+            "firstName" : firstName,
+            "lastName" : lastName,
+            "age" : age
+        };
+        
+        //aggiungo il nuovo studente all'array
+        studyGroup.push(newStudent);
+
+        //stampo il nuovo studente in pagina
+        document.getElementById("ninja_turtles").innerHTML += `
+        <li><strong>Nome:</strong>${firstName}</li>
+        <li><strong>Cognome:</strong> ${lastName}</li>
+        <hr>`
+    }
+);
